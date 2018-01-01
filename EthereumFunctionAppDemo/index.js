72548e980409159038c2f090dc6a3a857125ec52
Michael Chi <michi@microsoft.com>
2018-01-01 16:56:49 +0800

    move tx_url to app settings

--- EthereumFunctionAppDemo/index.js
+++ EthereumFunctionAppDemo/index.js
@@ -1,7 +1,8 @@
 module.exports = function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
-    var txURL = "http://52.234.144.43:8545";
+    var txURL = process.env.TX_URL;//"http://52.234.144.43:8545";
     if(req.method == 'GET'){
+        context.log(JSON.stringify(context.req));
         var Web3 = require('Web3');
         var web3 = new Web3();
         web3.setProvider(new web3.providers.HttpProvider(txURL));
